d8812d7426d836395fb5a4aecf4e1fbea5c20b50
memmm <emesenge@gmail.com>
2019-11-29 12:42:46 +0100

    Chapter constructor and name getter

---
+++ src/main/java/Chapter.java
@@ -0,0 +1,13 @@
+public class Chapter {
+    private String name;
+    private String number;
+
+    public Chapter(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
